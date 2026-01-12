d520a18cdcfc8f32cab8d69cf2b836341b3b2051
Your Name <you@example.com>
2026-01-12 13:35:34 -0500

    Finishing touches and bug fixes

--- Transformation_protocol.py
+++ Transformation_protocol.py
@@ -38,14 +38,14 @@ def run(protocol: protocol_api.ProtocolContext):
     transform = temp_adapter.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
 
     #Resevoir and Assemblies
-    resevoir = protocol.load_labware('opentrons_tough_4_reservoir_72ml', '11')
-    assemblies = protocol.load_labware('opentrons_tough_4_reservoir_72ml', '7')
+    resevoir = protocol.load_labware('opentrons_tough_4_reservoir_72ml', '2')
+    assemblies = protocol.load_labware('opentrons_tough_4_reservoir_72ml', '5')
 
     #Pipette and tips
-    p20_tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', '5')
+    p20_tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', '4')
     p20_multi = protocol.load_instrument('p20_multi_gen2', 'left', tip_racks=[p20_tiprack])
 
-    p300_tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', '4')
+    p300_tiprack = protocol.load_labware('opentrons_96_tiprack_300ul', '7')
     p300_multi = protocol.load_instrument('p300_multi_gen2', 'right', tip_racks=[p300_tiprack] )
 
     #Dilution Plate
@@ -55,7 +55,6 @@ def run(protocol: protocol_api.ProtocolContext):
     agar_plate_1 = protocol.load_labware('axygen_96_wellplate_500ul', '3')
     agar_plate_2 = protocol.load_labware('axygen_96_wellplate_500ul', '6')
     agar_plate_3 = protocol.load_labware('axygen_96_wellplate_500ul', '9')
-    agar_plate_4 = protocol.load_labware('axygen_96_wellplate_500ul', '2')
 
     #---------
     # Important Variables
@@ -67,11 +66,13 @@ def run(protocol: protocol_api.ProtocolContext):
     p300_multi.flow_rate.aspirate = 40
     p300_multi.flow_rate.dispense = 70
 
-    samples = 32
+    samples = 12
     sample_col = samples // 8
     assembly_wells = assemblies.wells()[0:sample_col:8]
-    recovery_wells = assembly_wells
-    trans_wells = recovery_wells
+    recovery_wells = recover.wells()[0:sample_col:8]
+    trans_wells = transform.wells()[0:sample_col:8]
+
+    plates = (agar_plate_1, agar_plate_2, agar_plate_3)
 
     dilution_wells = {}
     for i in range(sample_col):  # sample_col = number of 8-channel samples
@@ -91,31 +92,25 @@ def run(protocol: protocol_api.ProtocolContext):
         '''Create 96-well plates using tempered agar from resevoir.'''
         p300_multi.pick_up_tip()
 
-        for well in assembly_wells:
-            p300_multi.aspirate(location=resevoir.wells()[1], volume = plate_vol)
-            p300_multi.dispense(location = agar_plate_1.well(well))
-            p300_multi.blow_out(well.top())
-            p300_multi.aspirate(location=resevoir.wells()[1], volume = plate_vol)
-            p300_multi.dispense(location = agar_plate_2.well(well))
-            p300_multi.blow_out(well.top())
-            p300_multi.aspirate(location=resevoir.wells()[2], volume = plate_vol)
-            p300_multi.dispense(location = agar_plate_3.well(well))
-            p300_multi.blow_out(well.top())
-            p300_multi.aspirate(location=resevoir.wells()[2], volume = plate_vol)
-            p300_multi.dispense(location = agar_plate_4.well(well))
-            p300_multi.blow_out(well.top())
+        for i in range(sample_col):
+            for plate in plates:
+                dest = plate.columns()[i][0]
+                p300_multi.aspirate(location=resevoir.wells()[1], volume = plate_vol)
+                p300_multi.dispense(location = dest)
+                p300_multi.blow_out(dest.top())
         p300_multi.return_tip()
         
     def distribute_media(dilution_vol, recovery_vol):
         """Distribute media to dilution and recovery wells."""
         p300_multi.pick_up_tip()
-        for well in dilution_wells:
-            p300_multi.aspirate(location=resevoir.wells()[0], volume = dilution_vol)
-            p300_multi.dispense(location = dil_plate.well(well))
-            p300_multi.blow_out(well.top())
+        for i in range(sample_col):
+            for well in dilution_wells[i]:
+                p300_multi.aspirate(location=resevoir.wells()[0], volume = dilution_vol)
+                p300_multi.dispense(location = well)
+                p300_multi.blow_out(well.top())
         for well in recovery_wells:
             p300_multi.aspirate(location=resevoir.wells()[0], volume = recovery_vol)
-            p300_multi.dispense(location = recover.well(well))
+            p300_multi.dispense(location = well)
             p300_multi.blow_out(well.top())
         p300_multi.return_tip()
 
@@ -126,9 +121,10 @@ def run(protocol: protocol_api.ProtocolContext):
         :param assembly_vol: Volume to be transformed per well.
         '''
         for well in assembly_wells:
+            dest = transform.well(well.well_name)
             p20_multi.pick_up_tip()
-            p20_multi.aspirate(location= assemblies.well(well), volume = assembly_vol)
-            p20_multi.dispense(location = transform.well(well))
+            p20_multi.aspirate(location= well, volume = assembly_vol)
+            p20_multi.dispense(location = dest)
             p20_multi.blow_out(well.top())
             p20_multi.return_tip()
 
@@ -138,36 +134,38 @@ def run(protocol: protocol_api.ProtocolContext):
         
         :param trans_vol: Volume to be recovered per well.
         '''
-        for well in recovery_wells:
+        for well in trans_wells:
+            dest = recover.well(well.well_name)
             p20_multi.pick_up_tip()
-            p20_multi.aspirate(location= transform.well(well), volume = recover_vol)
-            p20_multi.dispense(location = recover.well(well))
+            p20_multi.aspirate(location= well, volume = recover_vol)
+            p20_multi.dispense(location = dest)
             p20_multi.blow_out(well.top())
-            p20_multi.return_tip
+            p20_multi.return_tip()
 
     def dilutions(dil1,dil2,dil3,dil4):
         '''Dilution of transformed samples.'''
-        for well in trans_wells:
-                index = trans_wells.index(well)
-                '''
-                Dilution order:
-                [1|3]
-                [2|4]
-                '''
-                p20_multi.pick_up_tip(p20_tiprack)
-                p20_multi.aspirate(location = recover.well(well), volume = dil1)
-                p20_multi.dispense(location = dil_plate.well(dilution_wells[index][0]))
-                p20_multi.mix(repetitions = 5)
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[index][0]), volume = dil2)
-                p20_multi.dispense(location = dil_plate.well(dilution_wells[index][1]))
-                p20_multi.mix(repetitions = 5)
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[index][1]), volume = dil3)
-                p20_multi.dispense(location = dil_plate.well(dilution_wells[index][2]))
-                p20_multi.mix(repetitions = 5)
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[index][2]), volume = dil4)
-                p20_multi.dispense(location = dil_plate.well(dilution_wells[index][3]))
-                p20_multi.mix(repetitions = 5)
-                p20_multi.return_tip()
+
+        for i in range(sample_col):
+            sample = recover.columns()[i][0]
+            '''
+            Dilution order:
+            [1|3]
+            [2|4]
+            '''
+            p20_multi.pick_up_tip()
+            p20_multi.aspirate(location = sample, volume = dil1)
+            p20_multi.dispense(location = dilution_wells[i][0])
+            p20_multi.mix(repetitions = 5, volume=3)
+            p20_multi.aspirate(location = dilution_wells[i][0], volume = dil2)
+            p20_multi.dispense(location = dilution_wells[i][1])
+            p20_multi.mix(repetitions = 5, volume=3)
+            p20_multi.aspirate(location = dilution_wells[i][1], volume = dil3)
+            p20_multi.dispense(location = dilution_wells[i][2])
+            p20_multi.mix(repetitions = 5, volume=3)
+            p20_multi.aspirate(location = dilution_wells[i][2], volume = dil4)
+            p20_multi.dispense(location = dilution_wells[i][3])
+            p20_multi.mix(repetitions = 5, volume=3)
+            p20_multi.return_tip()
 
     def plating():
         '''
@@ -176,27 +174,26 @@ def run(protocol: protocol_api.ProtocolContext):
         p20_multi.well_bottom_clearance.dispense = agar_height + 0.3
 
         for i in range(sample_col):
-                p20_multi.pick_up_tip(p20_tiprack)
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[i][0], volume = 3))
+                p20_multi.pick_up_tip()
+                p20_multi.aspirate(location = dilution_wells[i][1], volume = 3)
                 p20_multi.dispense(location = agar_plate_1.wells()[i])
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[i][1], volume = 3))
+                p20_multi.aspirate(location = dilution_wells[i][2], volume = 3)
                 p20_multi.dispense(location = agar_plate_2.wells()[i])
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[i][2], volume = 3))
+                p20_multi.aspirate(location = dilution_wells[i][3], volume = 3)
                 p20_multi.dispense(location = agar_plate_3.wells()[i])
-                p20_multi.aspirate(location = dil_plate.well(dilution_wells[i][3], volume = 3))
-                p20_multi.dispense(location = agar_plate_4.wells()[i],)
                 p20_multi.return_tip()
 
     def main():
         hs_mod.close_labware_latch()
 
+        #create agar plates
         create_plates(30)
 
-        #On ice
+        #on ice
         on_ice = temp_mod.start_set_temperature(celsius=4)
 
         #preheat heater/shaker
-        hs_mod.start_set_temperature(celsius=37)
+        hs_mod.set_target_temperature(celsius=37)
 
         #Distribute media into recovery and dilution wells
         distribute_media(60, 90)
@@ -206,23 +203,25 @@ def run(protocol: protocol_api.ProtocolContext):
         transformation(9)
         temp_mod.set_temperature(celsius=40)
         protocol.delay(seconds = 30)
+        temp_mod.set_temperature(celsius=4)
+        protocol.delay(minutes = 10)
         temp_mod.deactivate()
 
-        recovery(30)
-        # set Heater-Shaker temperature and shake speed
-        hs_mod.start_set_temperature(75)
+        #recovery profile
+        recovery(20)
+        hs_mod.set_and_wait_for_temperature(37)
         hs_mod.set_and_wait_for_shake_speed(300)
-
-        # create timer for sample incubation
         protocol.delay(minutes = 60)
-
-        # hold samples at target temperature
         hs_mod.deactivate_heater()
         hs_mod.deactivate_shaker()
 
-        dilutions()
+        #create dilutions
+        dilutions(3,3,3,3)
+        hs_mod.deactivate_heater()
 
+        #plate dilutions
         plating()
+        
 
     #run protocol
     main()
